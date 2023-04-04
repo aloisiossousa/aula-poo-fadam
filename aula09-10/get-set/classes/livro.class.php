08c77a8dadf62c55d98591bb5cc7a0bcc4b5c9d5
Aloisio S Sousa <aloisio.ssousa@gmail.com>
2023-04-04 10:30:24 -0300

    Exercício 5

---
+++ aula09-10/get-set/classes/livro.class.php
@@ -0,0 +1,40 @@
+
+<?php
+class Livro {
+    private $titulo;
+    private $autor;
+    private $editora;
+    private $ano;
+
+    public function getTitulo() {
+        return $this->titulo;
+    }
+
+    public function getAutor() {
+        return $this->autor;
+    }
+
+    public function getEditora() {
+        return $this->editora;
+    }
+
+    public function getAno() {
+        return $this->ano;
+    }
+
+    public function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    public function setAutor($autor) {
+        $this->autor = $autor;
+    }
+
+    public function setEditora($editora) {
+        $this->editora = $editora;
+    }
+
+    public function setAno($ano) {
+        $this->ano = $ano;
+    }
+}
