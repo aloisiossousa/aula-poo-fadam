e53d6aacfbd409b7492c4f6105494da8b94522d8
Aloisio S Sousa <aloisio.ssousa@gmail.com>
2023-04-18 20:12:40 -0300

    Criando Interface

--- aula11-12/interface/index.php
+++ aula11-12/interface/index.php
@@ -1,14 +1,22 @@
 <?php
-    interface MinhaInterface{
-        public const MINHACONSTANTE = " Oi a todos"; 
+    interface MinhaInterface1{
+    
+        public const MINHACONSTANTE = " Até a próxima aula.";
         public function metodo1();
+        public function metodo2();
+        public function metodo3();
+
+        
     }
 
     interface MinhaInterface2{
-        public function metodo2();
-        public function metodo3();
+        public function metodo4();
+        public function metodo5();
+        public function metodo6();
+
     }
-    class Classe implements MinhaInterface, MinhaInterface2{
+
+    class Classe implements MinhaInterface1,MinhaInterface2{
         public function metodo1(){
             echo "Olá ";
         }
@@ -18,12 +26,21 @@
         public function metodo3(){
             echo "Obrigado";
         }
+        public function metodo4(){
+            echo "Bom dia";
+        }
+        public function metodo5(){
+            echo "Boa tarde";
+        }
+        public function metodo6(){
+            echo "Boa noite";
+        }
  
     }
 
     $c = new Classe();
     $c->metodo1();
     $c->metodo2();
-    $c->metodo3();
+    $c->metodo6();
     echo $c::MINHACONSTANTE;
 ?>
